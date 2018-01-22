4c2bcfb33385f2d7589821a735b4a625fb43d94f
marsjoy <mme.mars.williams@udemy.com>
2018-01-22 00:02:28 -0800

    added recursive solution

--- challenges/hexadecimal.py
+++ challenges/hexadecimal.py
@@ -1,31 +1,39 @@
+HEXADECIMAL_PAIRS = {
+    '0': '0',
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    '10': 'A',
+    '11': 'B',
+    '12': 'C',
+    '13': 'D',
+    '14': 'E',
+    '15': 'F'
+}
 
-def convert(num):
-    hexadecimal_pairs = {
-        '0': '0',
-        '1': '1',
-        '2': '2',
-        '3': '3',
-        '4': '4',
-        '5': '5',
-        '6': '6',
-        '7': '7',
-        '8': '8',
-        '9': '9',
-        '10': 'A',
-        '11': 'B',
-        '12': 'C',
-        '13': 'D',
-        '14': 'E',
-        '15': 'F'
-    }
+def convert_to_hexadecimal(num):
     converted = []
     current = num
     quotient: int = True
     while quotient > 0:
         quotient, remainder = divmod(current, 16)
-        converted.append(hexadecimal_pairs[str(remainder)])
+        converted.append(HEXADECIMAL_PAIRS[str(remainder)])
         current = quotient
-    return ''.join(converted)
+    return ''.join(reversed(converted))
 
 
-
+def convert_to_hexadecimal_recursive(num):
+    is_negative = num < 0
+    num = abs(num)
+    if num >= 16:
+        quotient, remainder = divmod(num, 16)
+        b = convert_to_hexadecimal_recursive(quotient) + HEXADECIMAL_PAIRS[str(remainder)]
+    else:
+        b = HEXADECIMAL_PAIRS[str(num)]
+    return '-' + b if is_negative else b
--- tests/test_hexadecimal.py
+++ tests/test_hexadecimal.py
@@ -9,9 +9,16 @@ logger = logging.getLogger(__name__)
 class TestHexadecimal(TestCase):
 
     def test_convert_to_hexadecimal(self):
-        self.assertEqual(hexadecimal.convert_to_hexadecimal(254), 'EF')
-        # self.assertEqual(hexadecimal.convert_to_hexadecimal(2504), '9C8')
-        self.assertEqual(hexadecimal.convert_to_hexadecimal(50), '23')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal(254), 'FE')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal(2504), '9C8')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal(50), '32')
         self.assertEqual(hexadecimal.convert_to_hexadecimal(2), '2')
         self.assertEqual(hexadecimal.convert_to_hexadecimal(256), '100')
+
+    def test_convert_to_hexadecimal_recursive(self):
+        self.assertEqual(hexadecimal.convert_to_hexadecimal_recursive(254), 'FE')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal_recursive(2504), '9C8')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal_recursive(50), '32')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal_recursive(2), '2')
+        self.assertEqual(hexadecimal.convert_to_hexadecimal_recursive(256), '100')
 
